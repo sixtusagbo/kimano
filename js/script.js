67ca94099b8cbd84dba42e20ed0cc6073975208b
Markcrest-dev <cokafor169@gmail.com>
2023-04-24 16:21:57 -0700

    kimano

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// Add smooth scrolling to nav links
+document.querySelectorAll('nav a').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+  
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+        behavior: 'smooth'
+      });
+    });
+  });
