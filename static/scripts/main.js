725272b1842b94e0390aa919a2273568ecfd4958
sixtusagbo <miracleagbosixtus@gmail.com>
2023-04-26 14:50:40 +0100

    Redesign harmburger menu

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -24,4 +24,9 @@
     }
   });
 
+  const hamburger = document.querySelector(".hamburger");
+  hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle("flip");
+  });
+
 })(jQuery);
