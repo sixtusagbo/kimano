a2b292bdc072964164b3bad59fe5c2cef01ba071
sixtusagbo <miracleagbosixtus@gmail.com>
2023-05-02 01:57:15 +0100

    Add animation to harmbuger icon for mobile view

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -1,14 +1,22 @@
 (($) => {
-  'use strict';
+  "use strict";
 
-  $('.dapp_menu_item .dropdown_trigger').click(function (e) { 
+  $(".dapp_menu_item .dropdown_trigger").click(function (e) {
     e.preventDefault();
-    $('.dapp_menu_item').toggleClass('active');
+    $(".dapp_menu_item").toggleClass("active");
   });
 
-  $('#hamburger').click(function (e) { 
+  $("#hamburger").click(function (e) {
     e.preventDefault();
-    $('.primary_links').slideToggle();
-  });
+    $(".primary_links").slideToggle({
+      complete: () => {
+        $("html").toggleClass("our_mobile_menu");
 
+        if ($(".primary_links").css("display") == "block") {
+          // Because jquery toggles with display block and none
+          $(".primary_links").css("display", "flex");
+        }
+      },
+    });
+  });
 })(jQuery);
