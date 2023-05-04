f1b0259dd13c47e618c29009f58530430abb9aed
sixtusagbo <miracleagbosixtus@gmail.com>
2023-05-04 17:34:48 +0100

    Add dark mode feature

--- static/scripts/dapps_main.js
+++ static/scripts/dapps_main.js
@@ -25,5 +25,22 @@
     e.preventDefault();
     $(".connect_wallet_modal_popup").fadeOut();
   });
+
+  // Light and Dark mode
+  let currentTheme = localStorage["currentTheme"] || "light_mode";
+  $("body").removeClass("light_mode");
+  $("body").addClass(currentTheme);
+  $(".dropdown_dark_mode_toggle").click(function (e) { 
+    e.preventDefault();
+    if (localStorage.currentTheme == "light_mode") {
+      localStorage.currentTheme = "dark_mode";
+      $("body").removeClass("light_mode");
+      $("body").addClass(localStorage.currentTheme);
+    } else {
+      localStorage.currentTheme = "light_mode";
+      $("body").removeClass("dark_mode");
+      $("body").addClass(localStorage.currentTheme);
+    }
+  });
   
 })(jQuery);
